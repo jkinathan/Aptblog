6e2d90dfa3eb156283eb4fbc51b0bf89ebed1fd3
jkinobe <jkinobe@gmail.com>
2020-06-04 13:46:36 +0300

    post_delete

--- Blogger/Blog/views.py
+++ Blogger/Blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
 
 class BlogListView(ListView):
@@ -19,5 +20,10 @@ class BlogCreateView(CreateView):
     
 class BlogUpdateView(UpdateView):
     model = Post
-    template_name  = "post_edit.html"
+    template_name = "post_edit.html"
     fields = ['title','body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')
